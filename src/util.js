7e00155e023f28bcecc65a6caaaa812a052baaee
Joe Pell <jpell3130@gmail.com>
2019-06-12 23:26:14 -0400

    added randomVec and scale helper functions

--- src/util.js
+++ src/util.js
@@ -1,9 +1,20 @@
-const Util = {
+/* eslint-disable no-var */
+var Util = {
+  // sets up inheritance between a child and parent class
   inherits: function inherits(child, parent) {
     const Surrogate = function() {};
     Surrogate.prototype = parent.prototype;
     child.prototype = new Surrogate();
     child.prototype.constructor = child;
+  },
+  // returns a randomly generated vector given a length
+  randomVec: function randomVec(length) {
+    const deg = 2 * Math.PI * Math.random();
+    return Util.scale([Math.sin(deg), Math.cos(deg)], length);
+  },
+  // Scale the length of a vector by the given amount.
+  scale: function scale(vec, m) {
+    return [vec[0] * m, vec[1] * m];
   }
 };
 
