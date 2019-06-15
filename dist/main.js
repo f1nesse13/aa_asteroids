77ede2a3e7ecf37fb83b0cf3d8efc8ceee6d5092
Joe Pell <jpell3130@gmail.com>
2019-06-15 14:23:26 -0400

    webpack changed output

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,166 @@
-<<<<<<< HEAD
-!function(t){var o={};function e(n){if(o[n])return o[n].exports;var r=o[n]={i:n,l:!1,exports:{}};return t[n].call(r.exports,r,r.exports,e),r.l=!0,r.exports}e.m=t,e.c=o,e.d=function(t,o,n){e.o(t,o)||Object.defineProperty(t,o,{enumerable:!0,get:n})},e.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},e.t=function(t,o){if(1&o&&(t=e(t)),8&o)return t;if(4&o&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(e.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&o&&"string"!=typeof t)for(var r in t)e.d(n,r,function(o){return t[o]}.bind(null,r));return n},e.n=function(t){var o=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(o,"a",o),o},e.o=function(t,o){return Object.prototype.hasOwnProperty.call(t,o)},e.p="",e(e.s=4)}([function(t,o){function e(t){this.pos=t.pos,this.vel=t.vel,this.radius=t.radius,this.color=t.color,this.game=t.game}e.prototype.draw=function(t){t.beginPath(),t.arc(this.pos[0],this.pos[1],this.radius,0,2*Math.PI,!1),t.fillStyle=this.color,t.fill()},e.prototype.move=function(t){this.pos[0]+=this.vel[0],this.pos[1]+=this.vel[1],this.pos=this.game.wrap([this.pos[0],this.pos[1]])},t.exports=e},function(t,o){var e={inherits(t,o){const e=function(){};e.prototype=o.prototype,t.prototype=new e,t.prototype.constructor=t},randomVec(t){const o=2*Math.PI*Math.random();return e.scale([Math.sin(o),Math.cos(o)],t)},scale:(t,o)=>[t[0]*o,t[1]*o]};t.exports=e},function(t,o,e){const n=e(1),r=e(0);var i=function(t){r.call(this,{pos:t.pos,vel:n.randomVec(i.RADIUS),radius:i.RADIUS,color:i.COLOR,game:t.game})};n.inherits(i,r),i.RADIUS=40,i.COLOR="#a33",t.exports=i},function(t,o,e){const n=e(2);var r=function(){this.dim_x=r.DIM_X,this.dim_y=r.DIM_Y,this.num_asteroids=r.NUM_ASTEROIDS,this.asteroids=[],this.addAsteroids()};r.DIM_X=.75*window.innerWidth,r.DIM_Y=.75*window.innerHeight,r.NUM_ASTEROIDS=10,r.prototype.addAsteroids=function(){for(let t=0;t<r.NUM_ASTEROIDS;t++){const t=new n({pos:this.randomPosition(),game:this});this.asteroids.push(t)}},r.prototype.randomPosition=function(){return posX=Math.floor(Math.random()*r.DIM_X),posY=Math.floor(Math.random()*r.DIM_Y),[posX,posY]},r.prototype.draw=function(t){t.clearRect(0,0,r.DIM_X,r.DIM_Y);for(let o=0;o<this.asteroids.length;o++)this.asteroids[o].draw(t)},r.prototype.moveObjects=function(t){for(let o=0;o<this.asteroids.length;o++)this.asteroids[o].move(t);this.draw(t)},r.prototype.wrap=function(t){var o=t[0],e=t[1];return o<-5&&(o=.75*window.innerWidth+5),o>.75*window.innerWidth+5&&(o=0),e<-5&&(e=.75*window.innerHeight),e>.75*window.innerHeight+5&&(e=0),[o,e]},t.exports=r},function(t,o,e){e(0),e(2),e(3);var n=e(5);window.addEventListener("DOMContentLoaded",function(t){var o=document.getElementById("game_canvas");o.height=.75*window.innerHeight,o.width=.75*window.innerWidth;var e=o.getContext("2d");new n(e).start()})},function(t,o,e){const n=e(3),r=function(t){this.ctx=t,this.currentGame=new n};r.prototype.start=function(){game=this.currentGame,context=this.ctx,setInterval(function(){game.draw(context),game.moveObjects(context)},200)},t.exports=r}]);
-=======
-!function(t){var o={};function e(i){if(o[i])return o[i].exports;var n=o[i]={i:i,l:!1,exports:{}};return t[i].call(n.exports,n,n.exports,e),n.l=!0,n.exports}e.m=t,e.c=o,e.d=function(t,o,i){e.o(t,o)||Object.defineProperty(t,o,{enumerable:!0,get:i})},e.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},e.t=function(t,o){if(1&o&&(t=e(t)),8&o)return t;if(4&o&&"object"==typeof t&&t&&t.__esModule)return t;var i=Object.create(null);if(e.r(i),Object.defineProperty(i,"default",{enumerable:!0,value:t}),2&o&&"string"!=typeof t)for(var n in t)e.d(i,n,function(o){return t[o]}.bind(null,n));return i},e.n=function(t){var o=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(o,"a",o),o},e.o=function(t,o){return Object.prototype.hasOwnProperty.call(t,o)},e.p="",e(e.s=4)}([function(t,o,e){var i=e(1);function n(t){this.pos=t.pos,this.vel=t.vel,this.radius=t.radius,this.color=t.color,this.game=t.game}n.prototype.draw=function(t){t.beginPath(),t.arc(this.pos[0],this.pos[1],this.radius,0,2*Math.PI,!1),t.fillStyle=this.color,t.fill()},n.prototype.move=function(t){this.pos=this.game.wrap([this.pos[0]+=this.vel[0],this.pos[1]+=this.vel[1]])},n.prototype.isCollideWith=function(t){return i.distance(this.pos,t.pos)<this.radius+t.radius},n.prototype.collideWith=function(t){this.game.remove(this,t)},t.exports=n},function(t,o){var e={inherits(t,o){const e=function(){};e.prototype=o.prototype,t.prototype=new e,t.prototype.constructor=t},randomVec(t){const o=2*Math.PI*Math.random();return e.scale([Math.sin(o),Math.cos(o)],t)},scale:(t,o)=>[t[0]*o,t[1]*o],distance:(t,o)=>Math.sqrt((t[0]-o[0])**2+(t[1]-o[1])**2)};t.exports=e},function(t,o,e){const i=e(1),n=e(0);var s=function(t){n.call(this,{pos:t.pos,vel:i.randomVec(s.RADIUS),radius:s.RADIUS,color:s.COLOR,game:t.game})};i.inherits(s,n),s.RADIUS=40,s.COLOR="#a33",t.exports=s},function(t,o,e){var i=e(2),n=e(6),s=function(){this.dim_x=s.DIM_X,this.dim_y=s.DIM_Y,this.num_asteroids=s.NUM_ASTEROIDS,this.asteroids=[],this.ship=new n({pos:this.randomPosition(),game:this}),this.addAsteroids()};s.DIM_X=.75*window.innerWidth,s.DIM_Y=.75*window.innerHeight,s.NUM_ASTEROIDS=20,s.prototype.addAsteroids=function(){for(let o=0;o<s.NUM_ASTEROIDS;o++){var t=new i({pos:this.randomPosition(),game:this});this.asteroids.push(t)}return this.asteroids},s.prototype.allObjects=function(){this.addAsteroids().push(this.ship)},s.prototype.randomPosition=function(){return posX=Math.floor(Math.random()*s.DIM_X),posY=Math.floor(Math.random()*s.DIM_Y),[posX,posY]},s.prototype.draw=function(t){t.clearRect(0,0,s.DIM_X,s.DIM_Y);for(let o=0;o<this.allObjects.length;o++)this.asteroids[o].draw(t)},s.prototype.moveObjects=function(t){for(let o=0;o<this.asteroids.length;o++)this.asteroids[o].move(t);this.draw(t)},s.prototype.wrap=function(t){var o=t[0],e=t[1];return o<-5&&(o=.75*window.innerWidth+5),o>.75*window.innerWidth+5&&(o=0),e<-5&&(e=.75*window.innerHeight),e>.75*window.innerHeight+5&&(e=0),[o,e]},s.prototype.checkCollision=function(){for(let t=0;t<this.asteroids.length;t++)for(let o=0;o<this.asteroids.length;o++)t!==o&&this.asteroids[t].isCollideWith(this.asteroids[o])&&this.asteroids[t].collideWith(this.asteroids[o],this.asteroids[t])},s.prototype.step=function(t){this.moveObjects(t),this.checkCollision()},s.prototype.remove=function(t,o){this.asteroids.splice(this.asteroids.indexOf(t),1),this.asteroids.splice(this.asteroids.indexOf(o),1)},t.exports=s},function(t,o,e){e(0),e(2),e(3);var i=e(5);window.addEventListener("DOMContentLoaded",function(t){var o=document.getElementById("game_canvas");o.height=.75*window.innerHeight,o.width=.75*window.innerWidth;var e=o.getContext("2d");new i(e).start()})},function(t,o,e){const i=e(3),n=function(t){this.ctx=t,this.currentGame=new i};n.prototype.start=function(){game=this.currentGame,context=this.ctx,setInterval(function(){game.draw(context),game.step(context)},150)},t.exports=n},function(t,o,e){e(1)}]);
->>>>>>> object_collsion
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/asteroid.js":
+/*!*************************!*\
+  !*** ./src/asteroid.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("const Util = __webpack_require__(/*! ./util.js */ \"./src/util.js\");\nconst MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src/moving_object.js\");\n\nvar Asteroid = function(options) {\n  MovingObject.call(this, {\n    pos: options.pos,\n    vel: Util.randomVec(Asteroid.RADIUS),\n    radius: Asteroid.RADIUS,\n    color: Asteroid.COLOR,\n    game: options.game\n  });\n};\n\nUtil.inherits(Asteroid, MovingObject);\n\nAsteroid.RADIUS = 40;\nAsteroid.COLOR = '#a33';\n\nmodule.exports = Asteroid;\n\n\n//# sourceURL=webpack:///./src/asteroid.js?");
+
+/***/ }),
+
+/***/ "./src/game.js":
+/*!*********************!*\
+  !*** ./src/game.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("/* eslint-disable no-var */\n// Game class - bundles asteroids/player inside array and iterates over them to draw/move them\nvar Asteroid = __webpack_require__(/*! ./asteroid.js */ \"./src/asteroid.js\");\nvar Ship = __webpack_require__(/*! ./ship.js */ \"./src/ship.js\");\n\nvar Game = function() {\n  this.dim_x = Game.DIM_X;\n  this.dim_y = Game.DIM_Y;\n  this.num_asteroids = Game.NUM_ASTEROIDS;\n  this.asteroids = [];\n  this.ship = new Ship({ pos: this.randomPosition(), game: this });\n  this.addAsteroids();\n};\n\nGame.DIM_X = window.innerWidth * 0.75;\nGame.DIM_Y = window.innerHeight * 0.75;\nGame.NUM_ASTEROIDS = 20;\n\nGame.prototype.addAsteroids = function() {\n  // create new Asteroid Objects with a random position then pushes into the Game     instance's asteroids array\n  for (let i = 0; i < Game.NUM_ASTEROIDS; i++) {\n    var asteroid = new Asteroid({ pos: this.randomPosition(), game: this });\n    this.asteroids.push(asteroid);\n  }\n  return this.asteroids;\n};\n\nGame.prototype.allObjects = function() {\n  this.addAsteroids().push(this.ship);\n};\n\nGame.prototype.randomPosition = function() {\n  // Creates a random X and Y axis based off of the size of the canvas\n  posX = Math.floor(Math.random() * Game.DIM_X);\n  posY = Math.floor(Math.random() * Game.DIM_Y);\n  return [posX, posY];\n};\n\nGame.prototype.draw = function(ctx) {\n  // clears canvas\n  ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);\n  // redraws all asteroids inside array\n  for (let i = 0; i < this.allObjects.length; i++) {\n    this.asteroids[i].draw(ctx);\n  }\n};\n\nGame.prototype.moveObjects = function(ctx) {\n  // simulates movement of objects on canvas\n  for (let i = 0; i < this.asteroids.length; i++) {\n    this.asteroids[i].move(ctx);\n  }\n  this.draw(ctx);\n};\n\nGame.prototype.wrap = function(pos) {\n  var posX = pos[0];\n  var posY = pos[1];\n  if (posX < -5) {\n    posX = window.innerWidth * 0.75 + 5;\n  }\n  if (posX > window.innerWidth * 0.75 + 5) {\n    posX = 0;\n  }\n  if (posY < -5) {\n    posY = window.innerHeight * 0.75;\n  }\n  if (posY > window.innerHeight * 0.75 + 5) {\n    posY = 0;\n  }\n  return [posX, posY];\n};\n\nGame.prototype.checkCollision = function() {\n  for (let i = 0; i < this.asteroids.length; i++) {\n    for (let y = 0; y < this.asteroids.length; y++) {\n      if (i !== y && this.asteroids[i].isCollideWith(this.asteroids[y])) {\n        this.asteroids[i].collideWith(this.asteroids[y], this.asteroids[i]);\n      }\n    }\n  }\n};\n\nGame.prototype.step = function(ctx) {\n  this.moveObjects(ctx);\n  this.checkCollision();\n};\nGame.prototype.remove = function(obj, otherObj) {\n  this.asteroids.splice(this.asteroids.indexOf(obj), 1);\n  this.asteroids.splice(this.asteroids.indexOf(otherObj), 1);\n};\n\nmodule.exports = Game;\n\n\n//# sourceURL=webpack:///./src/game.js?");
+
+/***/ }),
+
+/***/ "./src/gameview.js":
+/*!*************************!*\
+  !*** ./src/gameview.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("const Game = __webpack_require__(/*! ./game.js */ \"./src/game.js\");\n// Keeps track of the objects inside the game's canvas and binds key handlers for players ship\n\nconst GameView = function(ctx) {\n  this.ctx = ctx;\n  this.currentGame = new Game();\n};\n\nGameView.prototype.start = function() {\n  game = this.currentGame;\n  context = this.ctx;\n  setInterval(function() {\n    game.draw(context);\n    game.step(context);\n  }, 150);\n};\n\nmodule.exports = GameView;\n\n\n//# sourceURL=webpack:///./src/gameview.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("/* eslint-disable no-unused-vars */\n/* eslint-disable vars-on-top */\n/* eslint-disable no-var */\nvar MovingObject = __webpack_require__(/*! ./moving_object */ \"./src/moving_object.js\");\nvar Asteroid = __webpack_require__(/*! ./asteroid.js */ \"./src/asteroid.js\");\nvar Game = __webpack_require__(/*! ./game.js */ \"./src/game.js\");\nvar GameView = __webpack_require__(/*! ./gameview.js */ \"./src/gameview.js\");\n\nwindow.addEventListener('DOMContentLoaded', function(e) {\n  var context = document.getElementById('game_canvas');\n  context.height = window.innerHeight * 0.75;\n  context.width = window.innerWidth * 0.75;\n  var ctx = context.getContext('2d');\n  // GameView grabs the canvas and starts to draw/move the objects\n  var newGame = new GameView(ctx);\n  newGame.start();\n});\n\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/moving_object.js":
+/*!******************************!*\
+  !*** ./src/moving_object.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("/* eslint-disable no-var */\nvar Util = __webpack_require__(/*! ./util.js */ \"./src/util.js\");\n\nfunction MovingObject(options) {\n  this.pos = options.pos;\n  this.vel = options.vel;\n  this.radius = options.radius;\n  this.color = options.color;\n  this.game = options.game;\n}\n\nMovingObject.prototype.draw = function(ctx) {\n  ctx.beginPath();\n  ctx.arc(this.pos[0], this.pos[1], this.radius, 0, Math.PI * 2, false);\n  ctx.fillStyle = this.color;\n  ctx.fill();\n};\nMovingObject.prototype.move = function(ctx) {\n  // sets position to wrap return value - if off canvas set to 0 || 'max'\n  this.pos = this.game.wrap([(this.pos[0] += this.vel[0]), (this.pos[1] += this.vel[1])]);\n};\n\nMovingObject.prototype.isCollideWith = function(otherObj) {\n  // Checks distances between objects to see if they collided\n  var distance = Util.distance(this.pos, otherObj.pos);\n  var collisionDistance = this.radius + otherObj.radius;\n\n  return distance < collisionDistance;\n};\n\nMovingObject.prototype.collideWith = function(otherObj) {\n  // function calls to remove both objects that have collided\n  this.game.remove(this, otherObj);\n};\n\nmodule.exports = MovingObject;\n\n\n//# sourceURL=webpack:///./src/moving_object.js?");
+
+/***/ }),
+
+/***/ "./src/ship.js":
+/*!*********************!*\
+  !*** ./src/ship.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("/* eslint-disable no-var */\nvar Util = __webpack_require__(/*! ./util.js */ \"./src/util.js\");\n\nvar Ship = function(options) {\n  MovingObject.call(this, {\n    pos: options.pos,\n    vel: Util.randomVec(0),\n    radius: Ship.RADIUS,\n    color: Ship.COLOR,\n    game: options.game\n  });\n\n  Util.inherits(Ship, MovingObject);\n  Ship.RADIUS = 25;\n  Ship.COLOR = '#ddd';\n};\n\n\n//# sourceURL=webpack:///./src/ship.js?");
+
+/***/ }),
+
+/***/ "./src/util.js":
+/*!*********************!*\
+  !*** ./src/util.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("var Util = {\n  // sets up inheritance between a child and parent class\n  inherits(child, parent) {\n    const Surrogate = function() {};\n    Surrogate.prototype = parent.prototype;\n    child.prototype = new Surrogate();\n    child.prototype.constructor = child;\n  },\n  // returns a randomly generated vector given a length\n  randomVec(length) {\n    const deg = 2 * Math.PI * Math.random();\n    return Util.scale([Math.sin(deg), Math.cos(deg)], length);\n  },\n  // Scale the length of a vector by the given amount.\n  scale(vec, m) {\n    return [vec[0] * m, vec[1] * m];\n  },\n  // calculate the distance between the two objects\n  distance(pos1, pos2) {\n    return Math.sqrt(((pos1[0] - pos2[0]) ** 2) + ((pos1[1] - pos2[1]) ** 2));\n  }\n};\n\nmodule.exports = Util;\n\n\n//# sourceURL=webpack:///./src/util.js?");
+
+/***/ })
+
+/******/ });
